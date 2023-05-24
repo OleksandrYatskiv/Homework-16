98eb0ccb9a010cc07f170be09e278af375ba394e
OleksandrYatskiv <yatsk1660@gmail.com>
2023-05-24 22:32:48 +0200

    Made some changes

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
 function removeElement(array, item) {
-    array.splice(array.indexOf(item), 1);
-
-    return array;
+    index = array.indexOf(item);
+    if (index !== -1) array.splice(index, 1);
 }
 
 removeElement(array, 2);
